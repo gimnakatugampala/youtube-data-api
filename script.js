0b9496467ffc4e3bf54ecac3e825e906d8d09717
Gimna Katugampala <gimnakatugampala1@gmail.com>
2020-12-25 17:06:10 +0530

    add

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function updateSigninStatus(isSignedIn){
         signoutButton.style.display = 'block';
         content.style.display = 'block';
         videoContainer.style.display = 'block';
-        getChannel(defaultChannel)
+        getChannel(defaultChannel);
     }else{
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
