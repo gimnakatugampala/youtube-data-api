7a946a204025420603e1d14eff3dff1bf6401344
Gimna Katugampala <gimnakatugampala1@gmail.com>
2020-12-25 18:28:52 +0530

    init

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'techguyweb';
+const defaultChannel = 'tseries';
 
 //Form submit and change channel
 channelForm.addEventListener('submit',e =>{
